615e03ad228de172baa723ba0313be5f8aa208fe
Kalyan Mudumby <71078441+theinhumaneme@users.noreply.github.com>
2023-06-08 23:54:43 +0530

    minigrep continues

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,27 @@
 use std::env;
-
+use std::error::Error;
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    dbg!(args);
+    if args.len() < 3 {
+        panic!("Not enough arguments");
+    }
+    let config = parse_args(&args);
+    run(config);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_args(args: &[String]) -> Config {
+    let query = args[0].clone();
+    let file_path = args[1].clone();
+    Config { query, file_path }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file_data = fs::read_to_string(config.file_path)?;
+    Ok(())
 }
